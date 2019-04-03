f1338d43ec026ca5959ea28b0c062e00d9dd3c50
dipto0321 <diptokmk47@gmail.com>
2019-04-03 15:43:37 +0600

    Remove some css code and modify app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            My first react App
-          </p>
-        </header>
+        <p>My first react App</p>
       </div>
     );
   }
